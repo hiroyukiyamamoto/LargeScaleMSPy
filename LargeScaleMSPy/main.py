54d067075db54167009a5b759e7633b5ed2621cf
hiroyuki yamamoto <h.yama2396@gmail.com>
2024-12-21 20:28:52 +0900

    Add files via upload

--- LargeScaleMSPy/main.py
+++ LargeScaleMSPy/main.py
@@ -1,16 +1,9 @@
-from .app import create_umap_app, load_umap_results, parse_msp_to_line_info
+from .create_umap_app import create_dash_app
+from .load_hdf5 import load_spectrum_from_hdf5
+from .pca_umap_analysis import perform_pca
+from .msp2hdf5 import convert_msp_to_hdf5
 
 def run_app():
-    # ファイルパスの設定
-    umap_results_path = "C:/Users/hyama/umap_results.npz"  # 必要に応じて調整
-    msp_file_path = "C:/Users/hyama/Documents/R/MSinfoR/MSDIAL-TandemMassSpectralAtlas-VS69-Pos.msp"
-
-    # 行範囲情報を生成
-    line_info = parse_msp_to_line_info(msp_file_path)
-
-    # UMAP結果を読み込み
-    umap_df = load_umap_results(umap_results_path)
-
-    # アプリケーションの作成と実行
-    app = create_umap_app(umap_df, msp_file_path, line_info, mz_max=2000)
+    # 必要なデータのロードや前処理
+    app = create_dash_app(umap_data, spectrum_loader)
     app.run_server(debug=True)
