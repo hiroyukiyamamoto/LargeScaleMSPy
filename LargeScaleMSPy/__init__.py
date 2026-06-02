59d0c05171bdd72a8289835377c561d972136cde
hiroyuki yamamoto <h.yama2396@gmail.com>
2026-06-02 16:07:22 +0900

    Delete LargeScaleMSPy directory

--- LargeScaleMSPy/__init__.py
+++
@@ -1,4 +0,0 @@
-# LargeScaleMSPy/__init__.py
-from .extract_mgf import massbank_to_mgf, parse_mgf_file, save_mgf_to_text
-from .pca_umap_analysis import preprocess_data, perform_pca, perform_umap
-from .vizapp import create_dash_app
--- LargeScaleMSPy/main.py
+++
@@ -1,9 +0,0 @@
-from .create_umap_app import create_dash_app
-from .load_hdf5 import load_spectrum_from_hdf5
-from .pca_umap_analysis import perform_pca
-from .msp2hdf5 import convert_msp_to_hdf5
-
-def run_app():
-    # 必要なデータのロードや前処理
-    app = create_dash_app(umap_data, spectrum_loader)
-    app.run_server(debug=True)
